18f2399e8a5d5ec5cdcdeeb206024fa183c5039c
yuce <hyucek@gmail.com>
2018-04-06 15:35:46 +0300

    initial push

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pluginDir="/usr/share/shutter/resources/system/plugins/shell/spocr/"
+
+#rootChecks
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+mkdir -p "$pluginDir"
+
+cp printParam "$pluginDir"
+cp spocr "$pluginDir"
+cp spocr.svg "$pluginDir"
+
+
+echo "Finished."
